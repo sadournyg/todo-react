d5cad9425e46cb2ea85b9293d723ac1ef1afb94c
Guillaume Sadourny <sadourny.guillaume@gmail.com>
2025-07-23 16:23:18 -0400

    feat: add todo form with priority and localStorage sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 type Priority = "Urgente" | "Moyenne" | "Basse";
 
 type Todo = {
@@ -7,9 +9,56 @@ type Todo = {
 };
 
 function App() {
+  const [input, setInput] = useState<string>("");
+  const [priority, setPriority] = useState<Priority>("Moyenne");
+  const savedTodos = localStorage.getItem("todos");
+  const initialTodos: Todo[] = savedTodos ? JSON.parse(savedTodos) : [];
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo() {
+    if (input.trim() == "") return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: input.trim(),
+      priority: priority,
+    };
+
+    const NewTodos = [newTodo, ...todos];
+    setTodos(NewTodos);
+    setInput("");
+    setPriority("Moyenne");
+  }
+
   return (
-    <div className="">
-      <button className="btn btn-accent">test</button>
+    <div className="flex justify-center">
+      <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
+        <div className="flex gap-4">
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Ajouter une tache..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <select
+            className="select w-full"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value as Priority)}
+          >
+            <option value="Urgente">Urgente</option>
+            <option value="Moyenne">Moyenne</option>
+            <option value="Basse">Basse</option>
+          </select>
+          <button onClick={addTodo} className="btn btn-primary">
+            Ajouter
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
